d275e818475aa339fe47b72ff8fc7845bf24103d
Peruzini <96450995+Peruzini@users.noreply.github.com>
2022-05-03 14:35:20 -0300

    Inicio de aula Resilia instalções de pacotes

---
+++ index.js
@@ -0,0 +1,14 @@
+// Todo Organizado
+// API  
+ const express = require('express');
+ const app = express ();
+
+ app.get('/',(req,res) => {
+     res.send("Hello World")
+ })
+
+ app.listen(5000, ()=>{
+
+    console.log("Rodando servidor  na porta 5000")
+ })
+
